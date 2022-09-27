791a2bf02786fba0cd786a4685f179b7433d8691
Eduardo De Jesus <eduardo.dejesus@itksquare.edu.mx>
2022-09-27 16:25:21 -0400

    finished the exercise

--- exercise.js
+++ exercise.js
@@ -15,7 +15,25 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const merged = (arrA, arrB) => {
     /* Only make changes below this comment */
-    
+    let mergedArray = [];
+    let i = 0, j = 0;
+    while((i<arrA.length)&&(j<arrB.length)){
+        if(arrA[i]<arrB[j]){
+            mergedArray.push(arrA[i]);
+            i++;
+        }else{
+            mergedArray.push(arrB[j]);
+            j++;
+        }
+    }    
+if(i<=(arrA.length-1)){
+        arrA.splice(0,i);
+        mergedArray=mergedArray.concat(arrA);
+    } else if(j<=(arrB.length-1)){
+        arrB.splice(0,j);
+        mergedArray=mergedArray.concat(arrB);
+    }
+return mergedArray;
     /* Only make changes below this comment */
 }
 
