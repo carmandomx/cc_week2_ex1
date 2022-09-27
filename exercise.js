fb442ebab359ff391591a8827359bf7d44c157ea
Nathaly Marrufo <99356378+nama59@users.noreply.github.com>
2022-09-27 15:49:42 -0500

    Work done

--- exercise.js
+++ exercise.js
@@ -15,9 +15,33 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const merged = (arrA, arrB) => {
     /* Only make changes below this comment */
-    
+    let mergedArr=[]; //A new array to concat both arrays
+    let temp; //A temp variable to use for the sort
+
+    //a loop to concat the arrays received in the empty one
+    for (let i = 0; i < arrA.length; i++) { 
+            mergedArr[i]=arrA[i];
+    }
+    for (let i = 0; i < arrB.length; i++) {
+        mergedArr[i+arrA.length]=arrB[i];
+    }
+     
+        let a=0; //A temp count for sorting purposes
+        do {
+              for (let i = 0; i < mergedArr.length; i++) { //A loop to run over all the array psitions
+          if (mergedArr[i]>mergedArr[i+1]) { //The conditional to sort the elements
+              temp=mergedArr[i+1]; //Temp use for saving the element deleted
+              mergedArr[i+1]= mergedArr[i];
+              mergedArr[i]=temp;
+                }
+          }  
+          a++;
+        } while (a==1); //To give a double check on the sort loop
+        
+    return mergedArr;
     /* Only make changes below this comment */
 }
+
 
 
 
