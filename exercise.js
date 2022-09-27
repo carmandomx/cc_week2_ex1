4220c11e7a5217063b21f90ce401c3864784ee99
Yumil Rueda <Yumil.Flores@itksquare.edu.mx>
2022-09-27 22:45:45 +0200

    test_1

--- exercise.js
+++ exercise.js
@@ -15,7 +15,24 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const merged = (arrA, arrB) => {
     /* Only make changes below this comment */
-    
+    let result = merged(arrA,arrB);
+
+    function bubblesrt(arr) {
+        let size = arr.length;
+      
+        for (let i = 0; i < size; i++) {
+          for (let j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+              temp = arr[j]
+              arr[j] = arr[j + 1]
+              arr[j + 1] = temp
+            }
+          }
+        }
+        return arr;
+      }
+      
+      result = bubblesrt(result);
     /* Only make changes below this comment */
 }
 
