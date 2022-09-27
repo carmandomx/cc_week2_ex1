099b8b1d5a433ea551091df11a621f86d05b74d9
EstrellaPoot <Estrella.Poot@itksquare.edu.mx>
2022-09-27 13:44:50 -0500

    Complete function to sort arrays

--- exercise.js
+++ exercise.js
@@ -15,7 +15,18 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const merged = (arrA, arrB) => {
     /* Only make changes below this comment */
-    
+let result = arrA
+
+arrB.forEach((element, idx) => {
+    const greatherIdx = result.findIndex((resultElement) => resultElement > element)
+    if (greatherIdx !== -1) {
+        result.splice(greatherIdx, 0, element)
+    } else {
+        result.push(element)
+    }
+})
+
+return result   
     /* Only make changes below this comment */
 }
 
