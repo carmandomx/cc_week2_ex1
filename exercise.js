e5397de64c7e062923172b1d0435d34de0a20625
Axcel Espinal <axcel.espinal@itksquare.edu.mx>
2022-09-27 10:48:47 -0400

    Solved the exercise

--- exercise.js
+++ exercise.js
@@ -15,6 +15,33 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const merged = (arrA, arrB) => {
     /* Only make changes below this comment */
+
+    const BubbleSort = (arr) => { //creates a bubble sort method to sort the resulting array
+        for (i=0; i<arr.length; i++) { //create a for loop to iterate through the array
+            for(j=i+1; j<arr.length; j++) { //create another for loop to be able to grab 2 elements at the same time
+                if(arr[i] > arr [j]) { //if our first element is higher than the next one..
+                    let temp = arr[i]; //save the first element in a temporal variable so its value is not lost
+                    arr[i] = arr[j]; //set our first element to have the value of the next, which is lesser
+                    arr[j] = temp; //set our second element to have the value of the first one, stored in the temporal variable
+                }
+            }
+        }
+        return arr;
+    }
+
+    let result = []; //creates an empty array to use as result
+    for(i=0; i<arrA.length; i++) { //for loop to insert every element on array A to our result array
+        result.push(arrA[i]);
+    }
+    for(j=0; j<arrB.length; j++) {//for loop to insert every element on array B to our result array
+        result.push(arrB[j]);
+    }
+
+    result = BubbleSort(result); //sort our result array with out Bubble Sort method
+
+
+    return result; //return the resulting array
+
     
     /* Only make changes below this comment */
 }
