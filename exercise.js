8ef78a5e5142b0834282d8d8274b48ab9b10d482
Tomas Garcia <Tomas.Garcia@itksquare.edu.mx>
2022-09-27 15:34:19 -0500

    Quick task done

--- exercise.js
+++ exercise.js
@@ -15,11 +15,38 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const merged = (arrA, arrB) => {
     /* Only make changes below this comment */
-    
+
+    let loopLength = arrA.length + arrB.length; // Total lenght of the array
+    let newArr = []; // Result array
+    //let temporary;
+
+    for (let i = 0; i <= loopLength; i++) { // Loop to get through over the array
+
+        if (arrA.length && arrB.length) { // Any lenght
+
+            if (arrA[0] <= arrB[0]) { // Compares positions
+                newArr = [...newArr,  newArr[i] = arrA.shift()]; // Do the shift positions
+            }
+
+            else {
+                newArr = [...newArr, newArr[i] = arrB.shift()];
+            }
+        }
+        
+        else if (arrA.length) {
+            newArr = [...newArr, newArr[i] = arrA.shift()];
+        }
+
+        else if (arrB.length) {
+            newArr = [...newArr, newArr[i] = arrB.shift()];
+        }
+    }
+    return newArr;
+
     /* Only make changes below this comment */
 }
 
-
+//console.log(merged([1, 2, 4], [1, 3, 4]));
 
 
 /** DO NOT CHANGE THE LINE BELOW **/
