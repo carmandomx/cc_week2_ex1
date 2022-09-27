b1f51d6cfc83288756246850f7203956f69b5dc8
LuisVazquezKsquare <luis.vazquez@itksquare.edu.mx>
2022-09-27 13:16:13 -0500

    Completed exercise 7

--- exercise.js
+++ exercise.js
@@ -15,11 +15,35 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const merged = (arrA, arrB) => {
     /* Only make changes below this comment */
+    //Firstly we must merge the arrays, we could merge them in one of them but let's stick to creating a new one
+    let sortedArray = [];
     
+    /*The concat() function let's us make a new array that is the ordered values of the
+    2 arrays, so we still need to sort them*/
+    sortedArray = arrA.concat(arrB);
+
+    //To sort the array we will use a bubble sort
+    for(let i = 0; i < sortedArray.length; i++){
+        for(let j = 0; j < sortedArray.length; j++){
+            if(sortedArray[i] < sortedArray[j]){
+                //We use a function for swaping the elements and sorting them
+                sortedArray = swapElements(sortedArray, i, j);
+                
+            }
+        }
+    }
+
+    //We just return the sorted array
+    return sortedArray;
     /* Only make changes below this comment */
 }
 
-
+const swapElements = (arra, indexA, indexB) => {
+    let temp = arra[indexA];
+    arra[indexA] = arra[indexB];
+    arra[indexB] = temp;
+    return arra;
+}
 
 
 /** DO NOT CHANGE THE LINE BELOW **/
