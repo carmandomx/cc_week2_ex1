5e10a6930acd369fde4ce671d7b494b1719bf951
Rodrigo Leon <rodrigo.leon@itksquare.edu.mx>
2022-09-27 15:31:04 -0500

    Exercise Completed

--- exercise.js
+++ exercise.js
@@ -11,16 +11,27 @@
 
 */
 
-
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const merged = (arrA, arrB) => {
-    /* Only make changes below this comment */
-    
-    /* Only make changes below this comment */
-}
+  /* Only make changes below this comment */
+  //Create a new const for union both array
+  const sortedArr = [...arrA, ...arrB];
 
-
-
+  //Create two for loops for iterate the array with two pointers
+  for (let i = 0; i < sortedArr.length; i++) {
+    for (let j = 0; j < sortedArr.length; j++) {
+      //Make the condition for when we want the swap is executed
+      if (sortedArr[i] < sortedArr[j]) {
+        //Create a new variable for help us to do the swap positions and do the swap
+        let temp = sortedArr[i];
+        sortedArr[i] = sortedArr[j];
+        sortedArr[j] = temp;
+      }
+    }
+  }
+  return sortedArr;
+  /* Only make changes below this comment */
+};
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.merged = merged;
