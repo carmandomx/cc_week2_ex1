e8acd13f08a86925e2930e3bca262eb372f1936a
EstrellaPoot <Estrella.Poot@itksquare.edu.mx>
2022-09-27 14:21:52 -0500

    Add comments

--- exercise.js
+++ exercise.js
@@ -15,17 +15,23 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const merged = (arrA, arrB) => {
     /* Only make changes below this comment */
+// Create variable and initialize it to the first received array
+// This variable will be actualized 
 let result = arrA
-
+// Evaluate second received array
 arrB.forEach((element, idx) => {
-    const greatherIdx = result.findIndex((resultElement) => resultElement > element)
+    // Find a greater element in the "result" array
+    const greatherIdx = result.findIndex((resultElement) => 
+    resultElement > element);
+    // if finds something different to "-1", then insert the element of second array in the position  "greaterIdx" in the "result" array without deleting any element
     if (greatherIdx !== -1) {
         result.splice(greatherIdx, 0, element)
     } else {
+        // if doesn't find a greater element, then just add the element to the "result" array
         result.push(element)
     }
 })
-
+// return the sorted array
 return result   
     /* Only make changes below this comment */
 }
