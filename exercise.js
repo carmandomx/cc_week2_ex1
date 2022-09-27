d4a0019e587cb0dcc8cdbacbb1375e832af03544
Eduardo De Jesus <eduardo.dejesus@itksquare.edu.mx>
2022-09-27 16:29:16 -0400

    added comments

--- exercise.js
+++ exercise.js
@@ -15,10 +15,10 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const merged = (arrA, arrB) => {
     /* Only make changes below this comment */
-    let mergedArray = [];
-    let i = 0, j = 0;
-    while((i<arrA.length)&&(j<arrB.length)){
-        if(arrA[i]<arrB[j]){
+    let mergedArray = []; //array where the other two will be saved
+    let i = 0, j = 0; //two separate counters, one for each array
+    while((i<arrA.length)&&(j<arrB.length)){ //running through each array
+        if(arrA[i]<arrB[j]){ //running through the arrays to see if they have the same length
             mergedArray.push(arrA[i]);
             i++;
         }else{
@@ -26,12 +26,12 @@ const merged = (arrA, arrB) => {
             j++;
         }
     }    
-if(i<=(arrA.length-1)){
-        arrA.splice(0,i);
-        mergedArray=mergedArray.concat(arrA);
-    } else if(j<=(arrB.length-1)){
-        arrB.splice(0,j);
-        mergedArray=mergedArray.concat(arrB);
+if(i<=(arrA.length-1)){ //we run the array backwards
+        arrA.splice(0,i); //we splice the first array
+        mergedArray=mergedArray.concat(arrA); //we add the elements of the first array
+    } else if(j<=(arrB.length-1)){ //we run the other array backwards too
+        arrB.splice(0,j); //we splice the 2nd array
+        mergedArray=mergedArray.concat(arrB); //we add the elements of the second array
     }
 return mergedArray;
     /* Only make changes below this comment */
