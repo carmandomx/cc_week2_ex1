47f92e2e3161385c115a719f03b93b3c607ff93f
Roberto_Tello <Roberto.Tello@itksquare.edu.mx>
2022-09-27 22:40:01 -0500

    commented

--- exercise.js
+++ exercise.js
@@ -15,7 +15,18 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const merged = (arrA, arrB) => {
     /* Only make changes below this comment */
-    
+    const arrC = arrA.concat(arrB);//we start by creating an array with the two past arrays
+for (let i = 0; i < arrC.length; i++) { //create a for loop inside a for loop   
+    for (let j = 0; j < arrC.length; j++) {
+        if (arrC[j] > arrC[j + 1]) {//we use a bubble sort and arrange everything from the smallest number to the biggest number 
+            let aux = arrC[j];
+            arrC[j] = arrC[j + 1];
+            arrC[j + 1] = aux;
+        } 
+    }
+}
+
+    return arrC;// we return the array rearranged
     /* Only make changes below this comment */
 }
 
