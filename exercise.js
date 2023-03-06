cf2531bb77f6a8baa5f5c1e1a46fec34fe638034
Andrei Santos <Andrei.Santos@itksquare.edu.mx>
2023-03-06 17:53:05 -0600

    cc_week2_ex1 AndreiSantos

--- exercise.js
+++ exercise.js
@@ -15,7 +15,18 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const merged = (arrA, arrB) => {
     /* Only make changes below this comment */
-    
+    let newArr = [];
+    let count = 0;
+    let concatArrs = arrA.concat(arrB);
+    while (newArr.length < concatArrs.length) {
+        for (let index = 0; index < concatArrs.length; index++) {
+            if (count === concatArrs[index]) {
+                newArr.push(concatArrs[index]);
+            }
+        }
+        count++
+    }
+    return newArr
     /* Only make changes below this comment */
 }
 
