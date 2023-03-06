8bb1bab31514a338369e5374e8a35f36adcb5ebb
Alexander Iván Suárez Vivas <alexander.suarez@itksquare.edu.mx>
2023-03-06 12:44:39 -0600

    Ejercicio resuelto

--- exercise.js
+++ exercise.js
@@ -11,16 +11,34 @@
 
 */
 
-
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const merged = (arrA, arrB) => {
-    /* Only make changes below this comment */
-    
-    /* Only make changes below this comment */
-}
+  /* Only make changes below this comment */
+  const mergedArray = [];
+  let i = 0;
+  let j = 0;
 
+  for (; i < arrA.length && j < arrB.length; ) {
+    if (arrA[i] < arrB[j]) {
+      mergedArray.push(arrA[i]);
+      i++;
+    } else {
+      mergedArray.push(arrB[j]);
+      j++;
+    }
+  }
 
+  for (; i < arrA.length; i++) {
+    mergedArray.push(arrA[i]);
+  }
 
+  for (; j < arrB.length; j++) {
+    mergedArray.push(arrB[j]);
+  }
+
+  return mergedArray;
+  /* Only make changes below this comment */
+};
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.merged = merged;
