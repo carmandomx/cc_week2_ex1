d68f0ee50322392610d2d4399e5afcc06484e70b
BraulioEstrada-Ksquare <Braulio.Estrada@itksquare.edu.mx>
2023-03-06 15:28:43 -0600

    ejercicio resuelto

--- exercise.js
+++ exercise.js
@@ -15,7 +15,22 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const merged = (arrA, arrB) => {
     /* Only make changes below this comment */
-    
+    let answer = [];
+    for(let i=0;i<arrA.length;i++){
+        answer.push(arrA[i]);
+
+    }
+    for(let i=0;i<arrB.length;i++){ //Two for loops that combine both arrays in the answer array
+        answer.push(arrB[i]);
+    }
+    for(let i=0; i<answer.length;i++){ //New for and nested for for making an bubblesort algorithm
+        for(let j=0; j<answer.length-i-1;j++){
+            if(answer[j]>answer[j+1]){ //comparison between a number in the array with the next one to the right, if value on left is greater they switch places
+                [answer[j],answer[j+1]] = [answer[j+1],answer[j]]; //they will continue switching places over and over again until the array is finaly sorted
+            }
+        }
+    }
+    return answer; // return the sorted array
     /* Only make changes below this comment */
 }
 
