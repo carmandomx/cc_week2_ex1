4457f5edccedd76541b4e89a5409213ce3be7e43
Rita Torres <taraumara96@gmail.com>
2023-03-13 16:55:41 -0600

    Add comments to the code

--- exercise.js
+++ exercise.js
@@ -17,19 +17,26 @@ const merged = (arrA, arrB) => {
   let suma = [];
   let compara1 = 0;
   let compara2 = 0;
+
+  //We take the first value of each array
   while (compara1 < arrA.length && compara2 < arrB.length) {
+    //We compare if arrA position is minor than arrB position
     if (arrA[compara1] < arrB[compara2]) {
+      //if arrA is minor we push to suma and increment de position of arrA 
         suma.push(arrA[compara1]);
       compara1++;
     } else {
+      //if arrB is minor we push to soma and increment de position of arrB
         suma.push(arrB[compara2]);
       compara2++;
     }
   }
+  //we add arrA to suma
   while (compara1 < arrA.length) {
     suma.push(arrA[compara1]);
     compara1++;
   }
+  //we add arrB to suma
   while (compara2 < arrB.length) {
     suma.push(arrB[compara2]);
     compara2++;
